4086a98f15861dfb50845490f2601d5ac9ee96e9
DobleA74 <andresandriani@gmail.com>
2022-12-13 23:28:31 -0300

    Agregue App.js y CSS

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-pwd
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use ( express.static(publicFolderPath));
+
+app.listen(3000, ()=>{
+    console.log('Servidor corriendo en el puerto 3000');
+})
