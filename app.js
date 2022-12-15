e5fb4dd3c9924708adbf0bbee1d0b1b4efd1bfec
DobleA74 <andresandriani@gmail.com>
2022-12-14 21:04:26 -0300

    actualizacion de app y html por imagenes

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const app = express();
 
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+app.use(express('public'));
 const publicFolderPath = path.resolve(__dirname, './public');
-app.use ( express.static(publicFolderPath));
+app.use (express.static(publicFolderPath));
 
-app.listen(3000, ()=>{
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(3030, ()=>{
+    console.log('Servidor corriendo en el puerto 3030');
 })
