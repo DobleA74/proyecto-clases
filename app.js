b0823aa6a5a0f2c774b2eb6a6b0e486ded9c2aae
DobleA74 <andresandriani@gmail.com>
2022-12-14 23:26:08 -0300

    agregue iconos y cumpli hasta penultimo desafio

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
-app.use(express('public'));
-const publicFolderPath = path.resolve(__dirname, './public');
-app.use (express.static(publicFolderPath));
+
+
 
 app.listen(3030, ()=>{
     console.log('Servidor corriendo en el puerto 3030');
