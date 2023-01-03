0d6f720eeb5131b799bb3073110e23353d3da2e5
DobleA74 <andresandriani@gmail.com>
2023-01-02 23:32:05 -0300

    Cree register y login

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ app.use(express.static('public'));
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
-
-
+app.get('/register', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get('/login', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.get('/home', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
 
 app.listen(3030, ()=>{
     console.log('Servidor corriendo en el puerto 3030');
